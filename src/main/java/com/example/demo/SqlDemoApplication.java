59023c93366b5282f400c4ecb7d4ab44c78c12c8
Sayali Thorat <sayali.thorat@v2force.com>
2019-06-13 17:04:27 +0530

    hi

--- src/main/java/com/example/demo/SqlDemoApplication.java
+++ src/main/java/com/example/demo/SqlDemoApplication.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,11 +13,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 public class SqlDemoApplication {
 
-	@RequestMapping("/hello")
-	String hello(Map<String, Object> model) {
-	  model.put("message", "Welcome to my app!");
-	  return "hello";
-	}
+	 @RequestMapping("/")
+	 String hello(Map<String, Object> model) {
+		  model.put("message", "Welcome to my app!");
+		  return "hello";
+		}
 	public static void main(String[] args) {
 		SpringApplication.run(SqlDemoApplication.class, args);
 	}
